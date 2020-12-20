f0d4b847ef746a45e41c8908821cee9ac8d30a94
RingoMar <justin.hernandez01@hotmail.com>
2020-12-20 15:32:25 -0400

    Quick Push from Sh

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,2 +1,2 @@
-let fs = requre('fs')
-console.log(JSON.parse(fs.readFileSync('./version.json', 'utf8')).version)
+let fs = require('fs')
+console.log(JSON.parse(fs.readFileSync('./module.json', 'utf8')).version)
